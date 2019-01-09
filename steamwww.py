1c1ba6c60bf34661f1fce7ce6dc64bd31c4b8fe7
Cyprian Guerra <cyprian.guerra@gmail.com>
2019-01-09 22:43:58 +0100

    added steamdb support as described in #17

--- steamwww.py
+++ steamwww.py
@@ -2,13 +2,17 @@
 import requests
 import sys
 import time
+import json
 from simplecache.cache import PickleCache
+
 
 class Scraper:
 
     def __init__(self):
         self.__cache = PickleCache("~/tmp/steamwww_cache.pkl", 86400 * 90)
         self.__calls = 0
+        with open('steamdb/GAMES.json') as steamdb:
+            self.__steamdb = json.load(steamdb)
 
     def get_html(self, id): # TODO: retire
         self.__calls += 1
@@ -36,18 +40,26 @@ class Scraper:
         assert r.status_code == 200, "Can't open %s (%d): %s" % (id, r.status_code, r.json())
         return j
 
-    def runs_on_linux(self, id, verbose = False):
+    def runs_on_linux(self, id, verbose = False, cache = True, steamdb = True):
         try:
+            if not cache:
+                raise KeyError
             linux = self.__cache.get(id)['linux']
             verbose and print('   ', end = '')
         except KeyError:
             try:
-                j = self.get_json(id)
-                linux = j['data']['platforms']['linux'] if j['data']['type'] == 'game' else True
+                if not steamdb:
+                    raise KeyError
+                linux = self.__steamdb[str(id)]
+                linux = True # TODO: filter hiddens, betas, etc.
             except KeyError:
-                linux = False
-            self.__cache.add(id, {"linux": linux})
-            self.__cache.save()
+                try:
+                    j = self.get_json(id)
+                    linux = j['data']['platforms']['linux'] if j['data']['type'] == 'game' else True
+                except KeyError:
+                    linux = False
+            cache and self.__cache.add(id, {"linux": linux})
+            cache and self.__cache.save()
             verbose and print(' + ', end = '')
         return linux
 
